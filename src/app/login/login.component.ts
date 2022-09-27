b97093964ae0b4b2272c7eaca7b8493125d1d6ae
Theon Pham <thanhpt24601@gmail.com>
2022-09-27 11:36:42 +0700

    Feat: Login form UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [CookieService],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: UntypedFormBuilder,
+    private cookieService: CookieService,
+    private router: Router
+  ) {}
+  formGroup: UntypedFormGroup = this.fb.group({
+    username: [null, [Validators.required]],
+    password: [null, [Validators.required]],
+  });
+  ngOnInit(): void {}
+  onLogin() {
+    const value = this.formGroup.getRawValue();
+    if (value.username == 123 && value.password == 123) {
+      this.cookieService.set('Login', 'true');
+    }
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MaterialModule],
-  exports: [MaterialModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
+  ],
+  exports: [MaterialModule, ReactiveFormsModule, FlexLayoutModule],
 })
 export class SharedModule {}
