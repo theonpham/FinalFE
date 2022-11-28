cc01eeac74f0717e46f6b4e12ca9cf80ea31939c
Theon Pham <thanhpt24601@gmail.com>
2022-11-28 12:03:24 +0700

    Feat : Update bill detail UI

--- src/app/bill/bill-detail/bill-detail.component.ts
+++ src/app/bill/bill-detail/bill-detail.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Inject, OnInit, TemplateRef, ViewChild, ViewContainerRef  } from '@angular/core';
+import { Component, Inject, OnInit, ViewContainerRef } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { BILL } from '../bill.const';
 import jsPDF from 'jspdf';
@@ -6,37 +6,34 @@ import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-bill-detail',
   templateUrl: './bill-detail.component.html',
-  styleUrls: ['./bill-detail.component.scss']
+  styleUrls: ['./bill-detail.component.scss'],
 })
-export class BillDetailComponent implements OnInit  {
-    // @ts-ignore
-    selectedBill : BILL;
-    pdfDisplay=true;
-     totalBill = 0;
+export class BillDetailComponent implements OnInit {
+  // @ts-ignore
+  selectedBill: BILL;
+  pdfDisplay = true;
+  totalBill = 0;
   constructor(
     @Inject(MAT_DIALOG_DATA)
-    public data: BILL,
-    private _dialog: MatDialog,
-    private vref:ViewContainerRef,
-  ) { }
+    public data: BILL
+  ) {}
   ngOnInit(): void {
     this.selectedBill = this.data;
-    this.selectedBill.foods.forEach((food)=>{
-      this.totalBill += food.price
-    })
+    console.log(this.selectedBill);
+
+    this.selectedBill.foods.forEach((food) => {
+      this.totalBill += food.price;
+    });
   }
   public openPDF(): void {
     let DATA: any = document.getElementById('htmlData');
     html2canvas(DATA).then((canvas) => {
-      console.log(canvas);
-      let fileWidth = 208;
+      let fileWidth = 95;
       let fileHeight = (canvas.height * fileWidth) / canvas.width;
-      const FILEURI = canvas.toDataURL('image/png');
-      let PDF = new jsPDF('p', 'mm');
-      let position = 0;
-      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      const FILEURI = canvas.toDataURL('image/png', 1.0);
+      let PDF = new jsPDF('p', 'mm', 'a6');
+      PDF.addImage(FILEURI, 'PNG', 5, 5, fileWidth, fileHeight);
       PDF.output('dataurlnewwindow');
     });
   }
 }
-
